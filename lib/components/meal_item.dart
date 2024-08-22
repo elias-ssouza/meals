db04318c4f5dd32982a9fab4cca6dd79886269e7
Elias Souza <elias.ssouza@hotmail.com>
2024-08-21 23:57:40 -0300

    fechando a tela e passando os dados de retorno

--- lib/components/meal_item.dart
+++ lib/components/meal_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/meal.dart';
-import 'package:meals/utils/app_routes.dart';
+import '../utils/app_routes.dart';
 
 class MealItem extends StatelessWidget {
   final Meal meal;
@@ -8,10 +8,18 @@ class MealItem extends StatelessWidget {
   const MealItem(this.meal, {Key? key}) : super(key: key);
 
   void _selectMeal(BuildContext context) {
-    Navigator.of(context).pushNamed(
+    Navigator.of(context)
+        .pushNamed(
       AppRoutes.MEAL_DETAIL,
       arguments: meal,
-      );
+    )
+        .then((result) {
+      if (result == null) {
+        print('Sem resultado!');
+      } else {
+        print('O nome da refeição é $result.');
+      }
+    });
   }
 
   @override
--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,51 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import '../models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
-  const MealDetailScreen({super.key});
+  const MealDetailScreen({Key? key}) : super(key: key);
 
-  _createSectionTitle(BuildContext context, String title){
+  Widget _createSectionTitle(BuildContext context, String title) {
     return Container(
-            margin: const EdgeInsets.symmetric(
-              vertical: 10
-            ),
-            child: Text(title,
-            style: Theme.of(context).textTheme.titleMedium,
-            ),
-          );
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.titleLarge,
+      ),
+    );
   }
 
-  Widget _createSectionContainer(Widget child){
+  Widget _createSectionContainer(Widget child) {
     return Container(
-            width: 300,
-            height: 200,
-             padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: child,
-        );
-    }
+      width: 330,
+      height: 200,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: child,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final meal = ModalRoute.of(context)?.settings.arguments as Meal;
+    final meal = ModalRoute.of(context)!.settings.arguments as Meal;
 
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
         title: Text(meal.title),
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: <Widget> [
-            Container(
+          children: [
+            SizedBox(
               height: 300,
               width: double.infinity,
               child: Image.network(
@@ -57,7 +53,7 @@ class MealDetailScreen extends StatelessWidget {
             _createSectionContainer(
               ListView.builder(
                 itemCount: meal.ingredients.length,
-                itemBuilder: (ctx, index){
+                itemBuilder: (ctx, index) {
                   return Card(
                     color: Theme.of(context).colorScheme.secondary,
                     child: Padding(
@@ -72,27 +68,36 @@ class MealDetailScreen extends StatelessWidget {
               ),
             ),
             _createSectionTitle(context, 'Passos'),
-            _createSectionContainer(
-              ListView.builder(
-                itemCount: meal.steps.length,
-                itemBuilder: (ctx, index){
-                  return Column(
-                    children: [
-                      ListTile(
-                        leading: CircleAvatar(
-                          child: Text('${index + 1}'),
+            _createSectionContainer(ListView.builder(
+              itemCount: meal.steps.length,
+              itemBuilder: (ctx, index) {
+                return Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        child: Text(
+                          '${index + 1}',
+                          style: const TextStyle(color: Colors.white),
                         ),
-                        title: Text(meal.steps[index]),
                       ),
-                      const Divider(),
-                    ],
-                  );
-                },
-              ),
-            )
+                      title: Text(meal.steps[index]),
+                    ),
+                    const Divider(),
+                  ],
+                );
+              },
+            )),
           ],
         ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        child: const Icon(Icons.star),
+        onPressed: () {
+          Navigator.of(context).pop(meal.title);
+        },
+      ),
     );
   }
 }
